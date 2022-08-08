b858d083295bf9f047ab09b3c2d9e26324483421
uwenorjeff <106762251+uwenorjeff@users.noreply.github.com>
2022-08-08 18:00:19 +0100

    Update 1-digit.c
    printf

--- 1-digit.c
+++ 1-digit.c
@@ -1,10 +1,10 @@
-#include "main.h" 
+#include "main.h"
 
 /**
  * init_params - clears struct fields and reset buf
  * @params: the parameters struct
  * @ap: the argument pointer
- * Return: void 
+ * Return: void
  */
 
 void init_params(params_t *params, va_list ap)
